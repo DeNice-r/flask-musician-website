df2b2b8aae241ec5f09d7410f5df527982af8bdb
DeNice-r <52076437+DeNice-r@users.noreply.github.com>
2021-11-12 15:48:18 +0200

    Final heroku db fix 1.

--- source/models/album.py
+++ source/models/album.py
@@ -10,3 +10,6 @@ class Album(db.Model):
 
     def __repr__(self):
         return f"{self.id=}, {self.image=}, {self.name=}, {self.year=}, {self.last_updated=}"
+
+
+db.create_all()
--- source/models/user.py
+++ source/models/user.py
@@ -9,3 +9,6 @@ class User(db.Model):
 
     def __repr__(self):
         return f"{self.id=}, {self.username=}, {self.password=}, {self.last_login=}"
+
+
+db.create_all()
